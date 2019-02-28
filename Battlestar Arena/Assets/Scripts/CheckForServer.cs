5d8ae30d21ff63ede4166939f8d618e0de8d4a3b
Mateja Lasan <mateja@lasan.ca>
2019-02-28 16:52:50 -0500

    Basic dedicated server functionality

---
+++ Battlestar Arena/Assets/Scripts/CheckForServer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CheckForServer : MonoBehaviour {
+	private void Start () {
+		if (Application.isBatchMode) {
+			Debug.Log("Application is running in Batch mode.");
+			Debug.Log("Starting server...");
+			gameObject.GetComponent<NetworkManager>().StartServer();
+			/* 
+			 * In the future, we should be using a more customized configuration depending on an associated file,
+			 * similarly to how Minecraft uses a server.properties file to load configuration data. In there we can
+			 * store public IP (if necessary), and port information as well as server password.
+			 */
+			Debug.Log("Server is running.");
+		} else {
+			Debug.Log("Application is running normally.");
+		}
+	}
+}
