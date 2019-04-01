b5e47f2498481ac672e27ae6dda8c70e383c54f2
Mateja Lasan <mateja@lasan.ca>
2019-03-31 20:22:28 -0400

    Verbose building to prevent timeout.

--- CIScripts/build.sh
+++ CIScripts/build.sh
@@ -7,7 +7,6 @@ echo "Attempting to build $project for Windows"
   -batchmode 
   -nographics 
   -silent-crashes 
-  -logFile $(pwd)/unity.log 
   -projectPath $(pwd) 
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" 
   -quit
@@ -17,7 +16,6 @@ echo "Attempting to build $project for OS X"
   -batchmode 
   -nographics 
   -silent-crashes 
-  -logFile $(pwd)/unity.log 
   -projectPath $(pwd) 
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" 
   -quit
@@ -27,14 +25,9 @@ echo "Attempting to build $project for Linux"
   -batchmode 
   -nographics 
   -silent-crashes 
-  -logFile $(pwd)/unity.log 
   -projectPath $(pwd) 
   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" 
   -quit
-
-echo 'Logs from build'
-cat $(pwd)/unity.log
-
 
 echo 'Attempting to zip builds'
 zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
